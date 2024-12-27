799bf375a9908cce067da4daa18dee3a8b860d1c
NewHorizonOfHope <sarsamurmu05+altgithub@gmail.com>
2024-12-27 13:16:03 +0530

    Update

---
+++ lb_lists.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+
+const load = async (name, filterRE, link) => {
+  let lines = (await(await fetch(link)).text()).trim().split('\n').map(x => x.trim())
+  lines = lines.filter(x => !x.startsWith('#') && (filterRE != null ? !filterRE.test(x) : 1))
+  fs.writeFileSync(`./out/lb_${name}.txt`, lines.join('\n'))
+}
+
+;(async () => {
+  await Promise.all([
+    load('proxy', /cleanbrowsing/, 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/doh-vpn-proxy-bypass-onlydomains.txt'),
+    load('unsafe_engines', null, 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/nosafesearch-onlydomains.txt')
+  ])
+})()
---
+++ run_all.js
@@ -0,0 +1,1 @@
+import './lb_lists.js'
