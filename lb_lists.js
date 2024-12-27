fe9e1dcb0f0c6eb33c35d60964aa658a22591b0a
NewHorizonOfHope <sarsamurmu05+altgithub@gmail.com>
2024-12-27 13:33:46 +0530

    Update

--- lb_lists.js
+++ lb_lists.js
@@ -10,6 +10,7 @@ const load = async (name, filterRE, link) => {
 ;(async () => {
   await Promise.all([
     load('proxy', /cleanbrowsing/, 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/doh-vpn-proxy-bypass-onlydomains.txt'),
-    load('unsafe_engines', null, 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/nosafesearch-onlydomains.txt')
+    load('unsafe_engines', null, 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/nosafesearch-onlydomains.txt'),
+    load('torrents', /(archive\.org)/, 'https://raw.githubusercontent.com/SM443/Pi-hole-Torrent-Blocklist/main/all-torrent-websites.txt')
   ])
 })()
